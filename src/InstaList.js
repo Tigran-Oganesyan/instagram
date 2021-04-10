bfa17d7dd36071b4234aef5586d081982e8b9be4
tigran <tigran@yandex.ru>
2021-04-10 20:39:43 +0300

    InstaList.js fixed

--- src/InstaList.js
+++ src/InstaList.js
@@ -6,17 +6,19 @@ import Form from './Form';
 export default function InstaList({title,image,cards,setCards,setImage,setTitle,setOpenedCardId}){
     return (
       <div className="insta">
-       <Form 
-         title={title} 
-         image={image} 
-         setImage={setImage} 
-         setTitle={setTitle} 
-         cards={cards} 
-         setCards={setCards}/>
-       <CardListInsta 
-         cards={cards} 
-         setCards={setCards} 
-         setOpenedCardId={setOpenedCardId}/>
+        <Form 
+          title={title} 
+          image={image} 
+          setImage={setImage} 
+          setTitle={setTitle} 
+          cards={cards} 
+          setCards={setCards}
+        />
+        <CardListInsta 
+          cards={cards} 
+          setCards={setCards} 
+          setOpenedCardId={setOpenedCardId}
+        />
       </div>
     )
 }
