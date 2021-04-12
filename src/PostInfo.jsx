4abfad6f8f8e52870a1af1f55d0de86c968c25f7
tigran <tigran@yandex.ru>
2021-04-12 13:24:12 +0300

    PostInfo.jsx

--- src/PostInfo.jsx
+++ src/PostInfo.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-export default function PostInfo({ post }){
-  
-    return (
-        <div className="post_info" key={post.id}>
-          <img className="post_img" src={post.image}></img>
-          <h2>{post.title}</h2>
-        </div>
-    )
+export default function PostInfo({ post }) {
+  return (
+    <div className="post_info" key={post.id}>
+      <img className="post_img" src={post.image} alt="" />
+      <h2>{post.title}</h2>
+    </div>
+  );
 }
