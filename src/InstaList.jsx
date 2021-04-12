f1ebcb65b7eb173cca940c81797eb105c3dd4ca7
tigran <tigran@yandex.ru>
2021-04-12 13:14:39 +0300

    InstaList.jsx style fixed

--- src/InstaList.jsx
+++ src/InstaList.jsx
@@ -3,22 +3,24 @@ import React from 'react';
 import CardListInsta from './CardListInsta';
 import Form from './Form';
 
-export default function InstaList({title,image,cards,setCards,setImage,setTitle,setOpenedCardId}){
-    return (
-      <div className="insta">
-        <Form 
-          title={title} 
-          image={image} 
-          setImage={setImage} 
-          setTitle={setTitle} 
-          cards={cards} 
-          setCards={setCards}
-        />
-        <CardListInsta 
-          cards={cards} 
-          setCards={setCards} 
-          setOpenedCardId={setOpenedCardId}
-        />
-      </div>
-    )
+export default function InstaList({
+  title, image, cards, setCards, setImage, setTitle, setOpenedCardId,
+}) {
+  return (
+    <div className="insta">
+      <Form
+        title={title}
+        image={image}
+        setImage={setImage}
+        setTitle={setTitle}
+        cards={cards}
+        setCards={setCards}
+      />
+      <CardListInsta
+        cards={cards}
+        setCards={setCards}
+        setOpenedCardId={setOpenedCardId}
+      />
+    </div>
+  );
 }
